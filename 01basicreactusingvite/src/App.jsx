95c18802d32be74df85bc4d9c45fc3849324e567
Shivam Mishra <91306181+MishraShivamm@users.noreply.github.com>
2023-10-07 06:17:45 +0000

    React using Vite

--- 01basicreactusingvite/src/App.jsx
+++ 01basicreactusingvite/src/App.jsx
@@ -4,10 +4,11 @@ import viteLogo from '/vite.svg'
 import Chai from './Chai'
 
 function App() {
+  const user = "Shivam "
   const [count, setCount] = useState(0)
 
   return (
-    <Chai/>
+    <><Chai /><h1>Chai  with {user}</h1></>
   )
 }
 
--- 01basicreactusingvite/src/main.jsx
+++ 01basicreactusingvite/src/main.jsx
@@ -3,8 +3,72 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+function Myapp(){
+  return(
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+
+/*const reactElement= {
+  type :"a",
+  props :{
+      href:"https://google.com",
+      target: "_blank"
+  },
+  children:"CLick me to visti Goggle"
+}*/
+
+
+const anotherUser = "Chai or react"
+const anotherElement = (
+  <a href='https://google.com' target = '_blank' >visit google</a>
 )
+
+const reactElement = React.createElement(
+  'a',
+  {href:"https://google.com", target:'_blank'},
+  'click me to visit google',
+  anotherUser 
+)
+ReactDOM.createRoot(document.getElementById('root')).render(
+ 
+    
+   reactElement
+    
+)
+
+/* Every react use a bunndler 
+JSX -JavaScript XML 
+ 
+this whole code "<div>
+      <h1>Custom App</h1>
+    </div>" is parsed and then converted into tree in this sort of code " const reactElement= {
+    type :"a",
+    props :{
+        href:"https://google.com",
+        target: "_blank"
+    },
+    children:"CLick me to visti Goggle"
+} "
+
+
+in react if you are creating element in main function "const reactElement = React.createElement()" like this so in this 
+creatElement there's a predefined way to declare things or give prarameter . 
+on first we have to declare a tag , 
+2nd an object,
+3rd will be direct object .
+variable inject (direct)
+
+
+in react when our tree is completed theen variable injections starts . 
+
+ const user = "Shivam " this is in a function and then 
+   return (
+    <><Chai /><h1>Chai  with {user}</h1></>
+  ) so  this " {user}" is called evaluated expression which will  show the final output . 
+
+
+*/
