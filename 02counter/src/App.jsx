a9e344bb2892f34145d695b0d4e1439ec0bc38d4
Shivam Mishra <91306181+MishraShivamm@users.noreply.github.com>
2023-10-08 06:07:04 +0000

    Hoooks

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,15 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let counter = 15
+
+  let [counter,setCounter]=useState(1)
+  //let counter = 15
  const addValue = () =>{
   console.log("clicked", counter);
-  counter =counter+1 
+  //counter =counter+1
+  setCounter(counter+1) 
   
   }
   const lessValue = () =>{
-    console.log("clicked", counter);
-    counter =counter - 1 
+    //Assignment part 
+    if(counter >1 ){
+      setCounter(counter-1)
+    }
+    else {
+      console.log("STOP")
+    }
+    // the above if else part is assignment for the hooks vedio of series Chai or React 
+   // counter =counter - 1 
     
     }
 
@@ -22,10 +32,10 @@ function App() {
       <h2>Counter Value:{counter} </h2>
       <button onClick={
         addValue
-      }>Add value</button>
+      }>Add value:{counter}</button>
       <><br /><button onClick={
         lessValue
-      }>less value</button></>
+      }>less value:{counter}</button></>
     </>
   )
 }
